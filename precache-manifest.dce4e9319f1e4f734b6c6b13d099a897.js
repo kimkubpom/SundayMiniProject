ef663dac37e2e39ddb624a83352c644ebb7d2b47
Kim Phanaphon <korawich_ice@hotmail.com>
2019-02-25 18:31:23 +0700

    Updates

---
+++ precache-manifest.dce4e9319f1e4f734b6c6b13d099a897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SundayMiniProject/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10b177ea6a8e08fd57bb",
+    "url": "/SundayMiniProject/static/js/runtime~main.10b177ea.js"
+  },
+  {
+    "revision": "b9a625033d6cc24e8440",
+    "url": "/SundayMiniProject/static/js/main.b9a62503.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/SundayMiniProject/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "b9a625033d6cc24e8440",
+    "url": "/SundayMiniProject/static/css/main.cf950102.chunk.css"
+  },
+  {
+    "revision": "0219649eef1fb4e251d524642ced13d4",
+    "url": "/SundayMiniProject/index.html"
+  }
+];
